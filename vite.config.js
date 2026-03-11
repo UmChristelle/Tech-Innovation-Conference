eaac6c2895ec780ce9565f6607f2257a71768dcd
Umchristelle <umutonichristella17@gmail.com>
2026-03-11 22:48:00 +0200

    fix the vite

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Tech-Innovation-Conference/",
+  base: process.env.NODE_ENV === 'production' 
+    ? '/Tech-Innovation-Conference/' 
+    : '/',
 })
